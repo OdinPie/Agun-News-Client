d57acdcd913ff723de116b027260797014b5917d
Maliha <maliha16tabassum@gmail.com>
2023-11-29 19:44:04 +0600

    declineModal working okay

--- src/Pages/Dashboard/AllArticles/ArticleRow.jsx
+++ src/Pages/Dashboard/AllArticles/ArticleRow.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 // import useAxiosPublic from "../../../Hooks/useArticles"
 import useArticles from '../../../Hooks/useArticles';
 import useAxiosPublic from '../../../Hooks/useAxiosPublic';

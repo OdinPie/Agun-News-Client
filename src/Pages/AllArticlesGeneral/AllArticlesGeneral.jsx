1b6c68927f97e0dc3b8edf639c8f101eb1a0d5be
Maliha <maliha16tabassum@gmail.com>
2023-11-29 21:29:15 +0600

    search and filter implemented

--- src/Pages/AllArticlesGeneral/AllArticlesGeneral.jsx
+++ src/Pages/AllArticlesGeneral/AllArticlesGeneral.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import useArticles from '../../Hooks/useArticles';
 import ArticleGeneral from './ArticleGeneral';
 import { useInfiniteQuery } from '@tanstack/react-query';
@@ -33,19 +33,35 @@ const animatedComponents = makeAnimated();
 //        </div>
 
 //     )
-    
+
+
    
 const AllArticlesGeneral = () => {
+    const [,refetch] = useArticles();
    const [articles] = useArticles();
    const publishers = usePublishers();
     const axiosPublic = useAxiosPublic();
+    const [showArticles, setShowArticles] = useState(articles);
+    const handleSubmit = e =>{
+        e.preventDefault();
+        const form = e.target;
+        const publisher = form.publisher.value;
+        const hint  = form.hint.value;
+        const rtags = form.tags.value;
+        const tags = rtags.split(' ');
+        // console.log(publisher, hint, tags);
+        axiosPublic.get(`/search?hint=${hint}&tags=${tags}&publisher=${publisher}`)
+        .then(res=>{
+            setShowArticles(res.data);
+        })
+    }
     return (
         <div>
             <h1 className='text-4xl font-bold font-play pt-40 text-center'>All Articles</h1><br /><br />
-            <form action="" className='flex justify-center items-center gap-6'>
+            <form onSubmit={handleSubmit} action="" className='flex justify-center items-center gap-6'>
             <div className="form-control">
       
-      <select className="select select-error w-full text-black" name='publisher' required>
+      <select className="select select-error w-full text-black" name='publisher' >
         <option disabled selected>Select Publishers</option>
         {
           publishers && publishers.map(pub=> <option>{pub.publication}</option>)
@@ -62,17 +78,17 @@ const AllArticlesGeneral = () => {
       placeholder='Select Tags'
       name='tags'
       delimiter=' '
-      required
+      
     />
     </div>
     <div className="form-control">
-    <input type="text" placeholder="Search here" className="input input-bordered input-error w-full text-black" />
+    <input type="text" name='hint' placeholder="Search here" className="input input-bordered input-error w-full text-black" />
     </div> 
     <button className='btn px-10 bg-red-500 text-white'>Search</button>
             </form><br /><br />
-            <div className='grid grid-cols-3 gap-5 max-w-7xl mx-auto'>
+            <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 max-w-7xl mx-auto'>
                 {
-                    articles && articles.map(art=>{
+                    showArticles && showArticles.map(art=>{
                         if(art.status==='approved'){
                             if(art.isPremium==='no'){
                                 return(<ArticleGeneral article={art}></ArticleGeneral>)

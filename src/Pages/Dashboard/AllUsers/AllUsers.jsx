39e8a2d49eba13d3c796e8fd7ab2c288c4aab602
Maliha <maliha16tabassum@gmail.com>
2023-12-01 14:55:45 +0600

    make admin function enabled

--- src/Pages/Dashboard/AllUsers/AllUsers.jsx
+++ src/Pages/Dashboard/AllUsers/AllUsers.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import useUsers from '../../../Hooks/useUsers'
 import UserRow from './UserRow';
 const AllUsers = () => {
-    const users  = useUsers();
+    const [users]  = useUsers();
     return (
         // TODO: name,email,profile picture, and Make admin button
         <div className='overflow-auto'>
-            <h1 className='text-4xl font-bold font-play pt-40 text-center'>All Articles</h1>
+            <h1 className='text-4xl font-bold font-play pt-40 text-center'>All Articles</h1><br />
             <div className="overflow-auto">
-                <table className="table table-small bg-slate-500">
+                <table className="table table-small">
                     <thead>
                     <tr className='text-white text-lg outline outline-white outline-2'>
                          
--- src/Pages/Dashboard/AllUsers/UserRow.jsx
+++ src/Pages/Dashboard/AllUsers/UserRow.jsx
@@ -1,14 +1,50 @@
 import React from 'react';
-
+import Swal from 'sweetalert2';
+import useUsers from '../../../Hooks/useUsers'
+import useAxiosPublic from '../../../Hooks/useAxiosPublic';
 const UserRow = ({user}) => {
     const { _id, displayName, email, photoURL } = user;
+    const [,refetch] = useUsers();
+    const axiosPublic = useAxiosPublic();
+    const makeAdmin = (id) =>{
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, make Admin"
+          }).then((result) => {
+            if (result.isConfirmed) {
+                axiosPublic.patch(`/user/makeadmin/${_id}`)
+                .then(res=>{
+                    console.log(res.data);
+                    if(res.data.modifiedCount > 0){
+                        refetch();
+                        Swal.fire({
+                            title: "Made Admin!",
+                            text: `${displayName} has been made admin!`,
+                            icon: "success"
+                          });
+                    }
+                })
+              
+            }
+          });
+        
+    }
     return (
         <tr>
 
             <td>{displayName}</td>
             <td>{email}</td>
             <td><img className='h-10' src={photoURL} alt="user" /></td>
-            <td><button className='btn bg-transparent outline-1 outline-red-700'>Make Admin</button></td>
+            <td>
+                {
+                    user?.role ? 'Admin' : <button onClick={()=>{makeAdmin(_id)}} className='btn bg-transparent text-white outline outline-1 outline-red-700'>Make Admin</button>
+                }
+            </td>
             
         </tr>
     );
